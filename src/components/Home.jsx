9986fdc96c5b55c618699aef5a4f4d2059763a28
anderson rangel <andergroundmsg@gmail.com>
2023-07-27 00:37:24 +1000

    detail

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,11 +5,9 @@ import Login from './Login/Login';
 const Home = () => {
   const currentUser = AuthService.getCurrentUser();
   return (
-    <>
-      <div className="container">
-        <div>{!currentUser ? <Login></Login> : <CardRow />}</div>;
-      </div>
-    </>
+    <div className="container">
+      <div>{!currentUser ? <Login></Login> : <CardRow />}</div>
+    </div>
   );
 };
 
