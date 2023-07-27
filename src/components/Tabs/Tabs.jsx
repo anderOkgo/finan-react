7b600f96c6252e7a0e579fb4d4e0446f08ebed11
anderson rangel <andergroundmsg@gmail.com>
2023-07-27 22:56:29 +1000

    remove .dard class

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -5,61 +5,59 @@ import './Tabs.css';
 
 export default function CardRow() {
   return (
-    <div className="card">
-      <div className="tabs">
-        <input className="radiotab" name={`tab-${1}`} type="radio" id={`tabone-${1}`} defaultChecked="checked" />
-        <label className="label" htmlFor={`tabone-${1}`}>
-          Tab1
-        </label>
-        <div className="panel">
-          <div className="tab-section">
-            <h2>{`Remainimg time`}</h2>
-            <hr />
-            <CountDownEnd />
-            <div>{/* <ExchangeRate /> */}</div>
-          </div>
+    <div className="tabs">
+      <input className="radiotab" name={`tab-${1}`} type="radio" id={`tabone-${1}`} defaultChecked="checked" />
+      <label className="label" htmlFor={`tabone-${1}`}>
+        Tab1
+      </label>
+      <div className="panel">
+        <div className="tab-section">
+          <h2>{`Remainimg time`}</h2>
+          <hr />
+          <CountDownEnd />
+          <div>{/* <ExchangeRate /> */}</div>
         </div>
-        <input className="radiotab" name={`tab-${1}`} type="radio" id={`tabtwo-${1}`} />
-        <label className="label tab-desc" htmlFor={`tabtwo-${1}`}>
-          Tab2
-        </label>
-        <div className="panel">
-          <div className="tab-section">
-            <h2>{'Title2'}</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quod laborum voluptates eaque nemo atque
-              necessitatibus laudantium ex error nisi esse facere, placeat nesciunt veritatis recusandae aliquam
-              itaque. Nesciunt, voluptatum!
-            </p>
-          </div>
+      </div>
+      <input className="radiotab" name={`tab-${1}`} type="radio" id={`tabtwo-${1}`} />
+      <label className="label tab-desc" htmlFor={`tabtwo-${1}`}>
+        Tab2
+      </label>
+      <div className="panel">
+        <div className="tab-section">
+          <h2>{'Title2'}</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quod laborum voluptates eaque nemo atque
+            necessitatibus laudantium ex error nisi esse facere, placeat nesciunt veritatis recusandae aliquam
+            itaque. Nesciunt, voluptatum!
+          </p>
         </div>
-        <input className="radiotab" name={`tab-${1}`} type="radio" id={`tabthree-${1}`} />
-        <label className="label tab-desc" htmlFor={`tabthree-${1}`}>
-          Tab3
-        </label>
-        <div className="panel">
-          <div id="section2" className="tab-section">
-            <h2>{'Title3'}</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi deleniti laudantium suscipit ipsam,
-              aliquid, rerum assumenda esse hic ducimus temporibus accusantium nesciunt quidem dolor ea delectus
-              deserunt sit! Repudiandae, quasi?
-            </p>
-          </div>
+      </div>
+      <input className="radiotab" name={`tab-${1}`} type="radio" id={`tabthree-${1}`} />
+      <label className="label tab-desc" htmlFor={`tabthree-${1}`}>
+        Tab3
+      </label>
+      <div className="panel">
+        <div id="section2" className="tab-section">
+          <h2>{'Title3'}</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi deleniti laudantium suscipit ipsam,
+            aliquid, rerum assumenda esse hic ducimus temporibus accusantium nesciunt quidem dolor ea delectus
+            deserunt sit! Repudiandae, quasi?
+          </p>
         </div>
-        <input className="radiotab" name={`tab-${1}`} type="radio" id={`tabfour-${1}`} />
-        <label className="label tab-desc" htmlFor={`tabfour-${1}`}>
-          Tab4
-        </label>
-        <div className="panel">
-          <div id="section2" className="tab-section">
-            <h2>{'Title4'}</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis consequuntur unde rem assumenda
-              distinctio labore impedit tenetur natus eos voluptatem quaerat vero veritatis dicta temporibus
-              maxime, error, soluta molestias perferendis?
-            </p>
-          </div>
+      </div>
+      <input className="radiotab" name={`tab-${1}`} type="radio" id={`tabfour-${1}`} />
+      <label className="label tab-desc" htmlFor={`tabfour-${1}`}>
+        Tab4
+      </label>
+      <div className="panel">
+        <div id="section2" className="tab-section">
+          <h2>{'Title4'}</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis consequuntur unde rem assumenda
+            distinctio labore impedit tenetur natus eos voluptatem quaerat vero veritatis dicta temporibus maxime,
+            error, soluta molestias perferendis?
+          </p>
         </div>
       </div>
     </div>
