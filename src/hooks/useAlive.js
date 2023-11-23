0131d55977a162f1b1026dc21d833ae46d3b0dfa
anderson rangel <andergroundmsg@gmail.com>
2023-11-23 13:31:51 +1000

    ofline avent handler

--- src/hooks/useAlive.js
+++ src/hooks/useAlive.js
@@ -24,7 +24,20 @@ export const useAlive = () => {
         setPrevInit(init);
       }
 
+      const handleOnline = () => {
+        setInit(true);
+      };
+
+      const handleOffline = () => {
+        setInit(false);
+      };
+
+      window.addEventListener('online', handleOnline);
+      window.addEventListener('offline', handleOffline);
+
       return () => {
+        window.removeEventListener('online', handleOnline);
+        window.removeEventListener('offline', handleOffline);
         clearTimeout(intervalId);
       };
     };
