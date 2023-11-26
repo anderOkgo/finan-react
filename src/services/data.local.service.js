070c931c37a011dabc784b581ff8ed8ec480a412
anderson rangel <andergroundmsg@gmail.com>
2023-11-26 19:27:05 +1000

    10 min check cookie

--- src/services/data.local.service.js
+++ src/services/data.local.service.js
@@ -1,6 +1,6 @@
 function createCookie(name, value) {
   const now = new Date();
-  const expirationTime = new Date(now.getTime() + 30 * 60 * 1000); // 30 minutes in milliseconds
+  const expirationTime = new Date(now.getTime() + 10 * 60 * 1000); // 10 minutes in milliseconds
   const cookieString = `${name}=${value}; expires=${expirationTime.toUTCString()}; path=/`;
   document.cookie = cookieString;
 }
