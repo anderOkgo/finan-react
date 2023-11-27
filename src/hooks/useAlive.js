ba5565c995a98c5c323e6747b7cae0d4f13f7cd0
anderson rangel <andergroundmsg@gmail.com>
2023-11-27 11:42:00 +1000

    refactor and restructure useAlive

--- src/hooks/useAlive.js
+++ src/hooks/useAlive.js
@@ -8,37 +8,28 @@ export const useAlive = () => {
   const [online, setOnline] = useState(true);
 
   useEffect(() => {
-    const fetchData = async () => {
-      if (online) {
-        if (!init) {
-          setProc(true);
-          const resp = await DataService.boot();
-          setInit(resp?.err ? false : true);
-          setProc(false);
-        }
-      }
-
-      init ? DataLocalService.createCookie('startCook', '1') : DataLocalService.deleteCookie('startCook');
-
-      const handleOnline = () => {
-        setOnline(true);
-      };
-
-      const handleOffline = () => {
-        setOnline(false);
-        setInit(false);
-      };
-
-      window.addEventListener('online', handleOnline);
-      window.addEventListener('offline', handleOffline);
-
-      return () => {
-        window.removeEventListener('online', handleOnline);
-        window.removeEventListener('offline', handleOffline);
-      };
+    const boot = async () => {
+      const resp = await DataService.boot();
+      setInit(resp?.err ? false : true);
     };
 
-    fetchData();
+    if (online && !init) setProc(true) & boot() & setProc(false);
+
+    init ? DataLocalService.createCookie('startCook', '1') : DataLocalService.deleteCookie('startCook');
+
+    const handleOnline = () => setOnline(true);
+    const handleOffline = () => setOnline(false) & boot();
+    const handleVisibilityChange = () => document.visibilityState === 'visible' && boot();
+
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOffline);
+    document.addEventListener('visibilitychange', handleVisibilityChange);
+
+    return () => {
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline);
+      document.removeEventListener('visibilitychange', handleVisibilityChange);
+    };
   }, [init, online]);
 
   return { init, setInit, proc, setProc };
