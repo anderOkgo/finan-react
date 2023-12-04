ebe507a6966c89374eb7f011afc5529eaa2fc9fc
anderson rangel <andergroundmsg@gmail.com>
2023-12-04 12:23:33 +1000

    modified sync to delete assest folder

--- com/db-sync-FTP-both.bat
+++ com/db-sync-FTP-both.bat
@@ -5,6 +5,7 @@ IF EXIST setter.bat (CD.. & CALL com\setter.bat) ELSE ( CALL com\setter.bat )
   /command ^
     "open ftp://%FTPUsername.txt%:%FTPPss.txt%@%FTPserver.txt%" ^
     "option batch off" ^
+    "rmdir  assets" ^
     "synchronize remote  ""%LocalPath.txt%\"" /%FTPFolder.txt%" ^
     "exit" ^
 IF %ERRORLEVEL% EQU 0 (Echo No error found) ELSE (Echo An error was found)
