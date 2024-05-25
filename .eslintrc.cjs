ddddfaa3661cf44a38a110756217baed98abb2b5
anderson rangel <andergroundmsg@gmail.com>
2024-05-24 20:10:18 -0500

    typescrpt cchanged

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,19 +2,17 @@
 
 module.exports = {
   env: { browser: true, es2020: true },
+  parser: '@typescript-eslint/parser',
   extends: [
-    'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
-}
+};
